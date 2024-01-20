e49f5f651554f7e1f0f96a028e6fa08108a79069
Jim Wyatt <jim@wyattconsulting.com>
2024-01-19 19:04:39 -0700

    Revert

--- app/src/main/java/com/example/FunctionLambdaRuntime.java
+++ app/src/main/java/com/example/FunctionLambdaRuntime.java
@@ -1,26 +1,18 @@
 package com.example;
 
 import java.net.MalformedURLException;
-
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-
 import io.micronaut.core.annotation.Nullable;
 import io.micronaut.function.aws.runtime.AbstractMicronautLambdaRuntime;
-import software.amazon.codeguruprofilerjavaagent.Profiler;
 
 public class FunctionLambdaRuntime extends AbstractMicronautLambdaRuntime<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent, APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     public static void main(String[] args) {
-
-        Profiler.builder()
-            .profilingGroupName("MicronautGraalProfilingGroup")
-            .withHeapSummary(true)
-            .build()
-            .start();
-
+        
         try {
+            
             new FunctionLambdaRuntime().run(args);
 
         } catch (MalformedURLException e) {
