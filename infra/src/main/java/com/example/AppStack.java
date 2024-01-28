f458877a9a70d6ffe02945fbd1c47258b46fbd7f
Jim Wyatt <jim@wyattconsulting.com>
2024-01-27 20:34:58 -0700

    Clean up

--- infra/src/main/java/com/example/AppStack.java
+++ infra/src/main/java/com/example/AppStack.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import io.micronaut.aws.cdk.function.MicronautFunction;
